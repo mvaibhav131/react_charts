0eb026a894a272cd4cb75ad11d4feeed01f15efa
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-06-26 17:09:30 +0530

    :100: app.js file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import BarLinePie from './component/BarLinePie';
+import Chart from './component/SimpleBar';
+
+function App() {
+  return (
+    <div className="App">
+    <h3>All Types Of Chart's</h3>
+     <Chart/>
+     <BarLinePie/>
+    </div>
+  );
+}
+
+export default App;
