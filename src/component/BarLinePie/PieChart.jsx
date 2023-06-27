9baa899ea5171fae6867c6b698d7d209ecc36b1f
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-06-27 16:40:27 +0530

    pie chart function for user gain

---
+++ src/component/BarLinePie/PieChart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Pie } from 'react-chartjs-2'
+
+const PieChart = ({chartData}) => {
+  return (
+    <div>
+     <div style={{width:330,margin:20}}>
+     <Pie data={chartData} options={{}} />
+     {/* <h2>Pie Chart</h2> */}
+    </div>
+    </div>
+  )
+};
+
+export default PieChart;
