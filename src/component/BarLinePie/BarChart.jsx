44f13c0d2eba2643f48e23869721aa1a6966fd8d
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-06-27 16:39:58 +0530

    bar function for user gain

---
+++ src/component/BarLinePie/BarChart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Bar } from 'react-chartjs-2';
+import {Chart as ChartJS} from "chart.js/auto" //is same as register chartjs
+
+const BarChart = ({chartData}) => {
+  return (
+    <div>
+      <div style={{width:500,margin:40}}>
+      <Bar data={chartData} options={{}}/>
+      {/* <h2>Bar Chart</h2> */}
+      </div>
+    </div>
+  )
+}
+
+export default BarChart;
