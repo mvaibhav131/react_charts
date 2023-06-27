60602576d6917a7d319a02e9ceb0d01c818312cd
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-06-27 16:38:36 +0530

    adding bar,line,pie

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import { useState } from 'react';
 import './App.css';
-import BarLinePie from './component/BarLinePie';
+import BarChart from './component/BarLinePie/BarChart';
 import Chart from './component/SimpleBar';
+import {userData} from "./Data";
+import LineChart from './component/BarLinePie/LineChart';
+import PieChart from './component/BarLinePie/PieChart';
 
 function App() {
+ 
+
+  const [data,setData]=useState({
+    labels:userData.map((data)=> data.year)  ,
+    datasets: [{
+      label:"Users Gained",
+      data:userData.map((data)=> data.userGain),
+      backgroundColor:["rgba(75,192,192,1)","aqua","#50AF95","#f3ba2f","black"],
+      borderColor:"black",
+      borderWidth:2
+    }]
+  });
+
   return (
     <div className="App">
     <h3>All Types Of Chart's</h3>
-     <Chart/>
-     <BarLinePie/>
+     <Chart/> 
+     <div style={{display:"flex",width:"auto"}}>
+     <BarChart chartData={data} />
+     <LineChart chartData={data} />
+     <PieChart chartData={data} />
+     </div>
+   
     </div>
   );
 }
