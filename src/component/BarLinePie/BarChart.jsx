58004df0643e966b6da49e3629be360ff821a886
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2024-01-02 10:37:54 +0530

    added bar function

--- src/component/BarLinePie/BarChart.jsx
+++ src/component/BarLinePie/BarChart.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Bar } from 'react-chartjs-2';
 import {Chart as ChartJS} from "chart.js/auto" //is same as register chartjs
 
+// Bar Chart Function 
 const BarChart = ({chartData}) => {
   return (
     <div>
